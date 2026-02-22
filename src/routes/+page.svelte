bcbe798503908652c9fe59da54057003f80cd830
ecanales23 <eycanal@mit.edu>
2026-02-21 23:02:46 -0500

    2.21.26

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Emily Canales</h1>
+<nav> 
+    <a href="index.html" class="current">Home</a>
+    <a href="src/routes/projects/index.html">Projects</a>
+    <a href="contact/index.html">Contact</a>
+    <a href="Resume/index.html">Resume</a>
+    <a href="https://github.com/ecanales23">GitHub</a>
+</nav> 
+<p>Hi there! My name is Emily Canales. I am a senior at MIT in Course 11 (Urban Planning). </p>
+<img src = "images/linkedin photo.jpg" alt = "Picture of myself">
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
